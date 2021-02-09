0d4974ed2934aaae80740ab14c575dd5bc13b8a4
Marcelo Frias <frias.marcelo@gmail.com>
2021-02-09 18:20:19 -0300

    hasta escribir un comentario

--- js/app.js
+++ js/app.js
@@ -1,9 +1,40 @@
 function campoRequerido(input){
     console.log("se ejecutó el onblur");
     //let input = document.querySelector("#nombre");
-if (input.value === "" || Number){
+if (input.value.trim() === ""){
     input.className="form-control is-invalid"
 } else {
     input.className= "form-control is-valid"
 }
+}
+
+function validarEmail(email){
+    console.log(email);
+let expresion = /\w+@\w+\.[a-z]{2,}$/;
+if (email.value.trim() != "" && expresion.test(email.value)){
+    email.className = "form-control is-valid"
+} else {
+    email.className = "form-control is-invalid"
+}
+}
+
+function validarNumeros(inputTelefono){
+    if (inputTelefono.value.trim() != "" && !isNaN(inputTelefono.value)){
+    inputTelefono.className = "form-control is-valid"
+} else {
+    inputTelefono.className = "form-control is-invalid"
+}
+}
+//isNam - esto no es un numero
+//devuelve true y false
+// ! dealnte de isNam invierte el valor de true y false
+
+function validarConsulta(texto){
+    if (texto.value.trim() != "" && texto.value.length >= 10){
+        texto.className = "form-control is-valid"
+    } else {
+        texto.className = "form-control is-invalid"
+    }
+
+    
 }
